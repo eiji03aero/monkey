19211f88ff2814103b6d151613cd82014c790321
eiji03aero <eiji03aero@gmail.com>
2019-01-26 16:34:32 +0900

    fin compiler Keeping Track of Names

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/eiji03aero/monkey/compiler"
 	"github.com/eiji03aero/monkey/lexer"
+	"github.com/eiji03aero/monkey/object"
 	"github.com/eiji03aero/monkey/parser"
 	"github.com/eiji03aero/monkey/vm"
 	"io"
@@ -15,6 +16,9 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -34,14 +38,17 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		comp := compiler.New()
+		comp := compiler.NewWithState(symbolTable, constants)
 		err := comp.Compile(program)
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Compilation failed: \n %s\n", err)
 			continue
 		}
 
-		machine := vm.New(comp.Bytecode())
+		code := comp.Bytecode()
+		constants = code.Constants
+
+		machine := vm.NewWithGlobalsStore(code, globals)
 		err = machine.Run()
 		if err != nil {
 			fmt.Fprintf(out, "Woops Executing failed:\n %s\n", err)
